e8c5a93b6ce37d16942b0f003026ebced22e562c
bakmaiev <thisyoungrhyme@gmail.com>
2023-10-08 20:53:49 +0300

    Create the Services component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "../Button/Button";
+import {
+  StyledBtnService,
+  StyledDescr,
+  StyledServices,
+  StyledServicesAction,
+  StyledServicesImg,
+  StyledServicesInfo,
+  StyledServicesMenu,
+  StyledServicesText,
+  StyledServicesWrapp,
+  StyledSubTitle,
+  StyledTitle,
+} from "./Services.styled";
+
+const servicesData = [
+  {
+    id: 1,
+    title: "Розробка ПЗ",
+    description:
+      "Наша досвідчена команда може розробити програмне забезпечення для широкого кола галузей промисловості та застосувань, зокрема сільського господарства, фінансів, охорони здоров’я та логістики тощо.",
+    image: require("../../images/service1.svg"),
+  },
+  {
+    id: 2,
+    title: "Створення ERP систем",
+    description:
+      "Розробка індивідуальних систем ERP (Enterprise Resource Planning) - системи, які оптимізують ваші бізнес-операції, покращують зв’язок між відділами та автоматизують процеси.",
+    image: require("../../images/service2.svg"),
+  },
+  {
+    id: 3,
+    title: "Створення інтернет магазину",
+    description:
+      "Ми пропонуємо послуги з розробки інтернет-магазину для клієнтів, які хочуть створити онлайн-версію свого бізнесу.",
+    image: require("../../images/service3.svg"),
+  },
+
+  {
+    id: 4,
+    title: "Підтримка та оновлення",
+    description:
+      "Ми пропонуємо постійну підтримку та оновлення, щоб гарантувати, що ваші програмні рішення залишаються актуальними та повністю функціональними.",
+    image: require("../../images/service4.svg"),
+  },
+
+  {
+    id: 5,
+    title: "Розробка ботів",
+    description:
+      "Ми спеціалізуємося на розробці індивідуальних ботів, які можуть автоматизувати завдання та підвищити ефективність.",
+    image: require("../../images/service5.svg"),
+  },
+
+  {
+    id: 6,
+    title: "Перенесення сайтів",
+    description:
+      "Ми пропонуємо послуги перенесення веб-сайту для клієнтів, які хочуть перенести свій веб-сайт з однієї платформи на іншу.",
+    image: require("../../images/service6.svg"),
+  },
+];
 
 const Services = () => {
-  return <div></div>;
+  const [selectedService, setSelectedService] = useState(servicesData[0]);
+
+  const handleServiceClick = (serviceId) => {
+    const service = servicesData.find((service) => service.id === serviceId);
+    setSelectedService(service);
+  };
+
+  return (
+    <StyledServices>
+      <StyledServicesWrapp>
+        <StyledTitle>Послуги</StyledTitle>
+        <StyledServicesMenu>
+          {servicesData.map((service) => (
+            <li key={service.id} onClick={() => handleServiceClick(service.id)}>
+              <StyledBtnService type="button">{service.title}</StyledBtnService>
+            </li>
+          ))}
+        </StyledServicesMenu>
+        <StyledServicesInfo>
+          <StyledServicesText>
+            <StyledSubTitle>{selectedService.title}</StyledSubTitle>
+            <StyledDescr>{selectedService.description}</StyledDescr>
+            <Button>Хочу</Button>
+          </StyledServicesText>
+          <StyledServicesAction>
+            <StyledServicesImg
+              src={selectedService.image.default}
+              alt={selectedService.title}
+            ></StyledServicesImg>
+          </StyledServicesAction>
+        </StyledServicesInfo>
+      </StyledServicesWrapp>
+    </StyledServices>
+  );
 };
 
 export default Services;
---
+++ src/components/Services/Services.styled.js
@@ -0,0 +1,158 @@
+import styled from "styled-components";
+
+export const StyledServices = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 16px;
+  background: linear-gradient(
+    39deg,
+    rgba(120, 137, 153, 0.106516106442577) 0%,
+    rgba(140, 150, 164, 0.10211834733893552) 26%,
+    rgba(161, 164, 175, 0.1211834733893552) 50%
+  );
+`;
+
+export const StyledServicesMenu = styled.div`
+  max-width: 100%;
+  display: flex;
+  align-items: center;
+  gap: 32px;
+  overflow-x: auto;
+  white-space: nowrap;
+  margin-bottom: 32px;
+  padding: 0 0 8px 0;
+
+  &::-webkit-scrollbar {
+    width: 16px;
+    height: 4px;
+  }
+
+  &::-webkit-scrollbar-thumb {
+    background: #a1a4af;
+  }
+
+  &::-webkit-scrollbar-track {
+    background: linear-gradient(
+      39deg,
+      rgba(120, 137, 153, 0.106516106442577) 0%,
+      rgba(140, 150, 164, 0.10211834733893552) 26%,
+      rgba(161, 164, 175, 0.1211834733893552) 50%
+    );
+  }
+`;
+
+export const StyledServicesWrapp = styled.div`
+  max-width: 1200px;
+  width: -webkit-fill-available;
+  padding: 0 16px;
+  margin: 0 auto;
+`;
+
+export const StyledBtnService = styled.button`
+  position: relative;
+  width: 250px;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #000;
+  color: #fff;
+  cursor: pointer;
+  overflow: hidden;
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 16px;
+  transition: all 0.25s ease-in-out;
+
+  &::before,
+  &::after {
+    content: "";
+    position: absolute;
+    box-sizing: border-box;
+    transition: all 0.25s ease-in-out;
+    width: 100%;
+    height: 100%;
+    z-index: 1;
+  }
+
+  &::before {
+    opacity: 0;
+    background: linear-gradient(
+      39deg,
+      rgba(120, 137, 153, 0.106516106442577) 0%,
+      rgba(140, 150, 164, 0.10211834733893552) 26%,
+      rgba(161, 164, 175, 0.1211834733893552) 50%
+    );
+    transform: scale(0.1, 0.1);
+  }
+
+  &:hover::before {
+    opacity: 1;
+    transform: scale(1, 1);
+  }
+
+  @media screen and (min-width: 1200px) {
+    width: calc((1200px - 96px) / 4);
+  }
+`;
+
+export const StyledServicesInfo = styled.div`
+  max-width: 100%;
+  height: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+  background-color: #000;
+  padding: 32px;
+
+  @media screen and (min-width: 1024px) {
+    flex-direction: row;
+  }
+`;
+
+export const StyledServicesText = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+`;
+
+export const StyledServicesAction = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 32px;
+`;
+
+export const StyledTitle = styled.h2`
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+  margin-bottom: 16px;
+`;
+
+export const StyledSubTitle = styled.h3`
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 24px;
+  font-weight: bold;
+  color: #fff;
+  text-align: center;
+`;
+
+export const StyledDescr = styled.p`
+  font-family: "Nunito Sans", sans-serif;
+  font-size: 16px;
+  font-weight: medium;
+  text-align: center;
+  color: #fff;
+`;
+
+export const StyledServicesImg = styled.img`
+  max-width: 400px;
+  width: 100%;
+  height: fit-content;
+  object-fit: contain;
+`;
